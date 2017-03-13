d3e9a700f9a62e2047baaf5d4dd4425268b5e16e
d-graf <dario.graf@comvation.com>
2017-03-13 16:17:31 +0100

    Add image upload validation

--- view/image_upload.php
+++ view/image_upload.php
@@ -5,7 +5,17 @@
 
             <?php
             $form = new Form('/image/doUpload');
+            if(isset($_SESSION["errorTitle"])){
+                echo $_SESSION["errorTitle"];
+                unset($_SESSION['errorTitle']);
+            }
+            echo '<p style="font-style:italic;">The title can in maximum contain 10 chars!</p>';
             echo $form->text()->name('title')->placeholder('title')->type('text');
+            if(isset($_SESSION["errorImage"])){
+                echo $_SESSION["errorImage"];
+                unset($_SESSION['errorImage']);
+            }
+            echo '<p style="font-style:italic;">The filetype must be: png, jpg, jpeg!</p>';
             echo $form->text()->name('image')->placeholder('image')->type('file');
             echo $form->submit()->label('Post')->name('post');
             $form->end();
