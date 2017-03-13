e046565f91ea737f6062f6da185b31b51e90c226
d-graf <dario.graf@comvation.com>
2017-03-13 13:53:29 +0100

    Add logo in navbar

--- view/header.php
+++ view/header.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
     <link rel="stylesheet" href="/css/style.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <title>WatchMeUp</title>
 </head>
 <body>
 <header>
     <nav>
         <ul>
-            <li><a href="/">WatchMeUp</a></li>
+            <li><a href="/"><img id="logo" src="/images/logo_watchmeup.png"/></a></li>
                 <?php if (Security::isLoggedIn()) : ?>
                     <li><a href="/image/upload">Upload</a></li>
                     <li><a href="/user/logout">Logout</a></li>
